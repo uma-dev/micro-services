a7cdd91a82cad1acae017de457e7c4c9b95b3ce9
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-07-18 14:39:35 -0600

    Added entity

---
+++ worker/src/main/java/com/umadev/worker/entity/Worker.java
@@ -0,0 +1,21 @@
+package com.umadev.worker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Worker{
+    @Id
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer departmentId;
+}
