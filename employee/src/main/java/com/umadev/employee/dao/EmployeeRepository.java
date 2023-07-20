629b9b3ad82929844943bca3c2bfb61ef64cf852
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-07-20 14:39:12 -0600

    Method findAllByDepartmentId (implemented by JPA) added

--- employee/src/main/java/com/umadev/employee/dao/EmployeeRepository.java
+++ employee/src/main/java/com/umadev/employee/dao/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.umadev.employee.dao;
 import com.umadev.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAllByDepartmentId(Integer departmentId);
 }
