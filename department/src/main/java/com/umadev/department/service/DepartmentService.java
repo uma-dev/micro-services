9b5be72e68f39b34b739644840ae13674c92f8e5
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-07-20 14:27:31 -0600

    Method findDepartmentsWithEmployees added

--- department/src/main/java/com/umadev/department/service/DepartmentService.java
+++ department/src/main/java/com/umadev/department/service/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.umadev.department.service;
 
 import com.umadev.department.entity.Department;
+import com.umadev.department.entity.FullDepartmentResponse;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface DepartmentService {
     List<Department> findAll();
 
     Department save(Department theDepartment);
+
+    FullDepartmentResponse findDepartmentsWithEmployees(Integer employeeId);
 }
--- department/src/main/java/com/umadev/department/service/DepartmentServiceImpl.java
+++ department/src/main/java/com/umadev/department/service/DepartmentServiceImpl.java
@@ -1,7 +1,9 @@
 package com.umadev.department.service;
 
+import com.umadev.department.client.EmployeeClient;
 import com.umadev.department.dao.DepartmentRepository;
 import com.umadev.department.entity.Department;
+import com.umadev.department.entity.FullDepartmentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,12 @@ import java.util.List;
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
     private final DepartmentRepository departmentRepository;
+    // Dependency injected
+    private final EmployeeClient employeeClient;
     @Autowired
-    public DepartmentServiceImpl(DepartmentRepository theDepartmentRepository){
+    public DepartmentServiceImpl(DepartmentRepository theDepartmentRepository, EmployeeClient theEmployeeClient){
         departmentRepository = theDepartmentRepository;
+        employeeClient = theEmployeeClient;
     }
 
     @Override
@@ -23,5 +28,23 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Override
     public Department save(Department theDepartment){
         return departmentRepository.save(theDepartment);
+    }
+
+    @Override
+    public FullDepartmentResponse findDepartmentsWithEmployees(Integer employeeId){
+        // var is used because Java guess the type of department (Department)
+        var department = departmentRepository.findById(employeeId)
+                .orElse(
+                        Department.builder()
+                                .name("NOT_FOUND")
+                                .email("NOT_FOUND")
+                                .build()); // avoid null pointer exceptions
+        // Find all the students from the student client with OpenFeign
+        var employees = employeeClient.findAllEmployeesByDepartment(employeeId);
+        return  FullDepartmentResponse.builder()
+                .name(department.getName())
+                .email(department.getEmail())
+                .employees(employees)
+                .build();
     }
 }
