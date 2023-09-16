657b25f9fd5e75f5a503f4b6b8a446686e07f8c5
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-09-15 20:58:14 -0600

    Added PUT and DELETE tests

--- department/src/test/java/com/umadev/department/controller/DepartmentControllerTest.java
+++ department/src/test/java/com/umadev/department/controller/DepartmentControllerTest.java
@@ -7,12 +7,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.umadev.department.entity.Department;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.cli.CliDocumentation;
@@ -32,7 +34,10 @@ import java.util.stream.Stream;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 // To work with a JUnit 5 test, we have to extend the test with the RestDocumentationExtension class
 @ExtendWith({ RestDocumentationExtension.class, SpringExtension.class})
@@ -40,7 +45,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // Generate documentation
 @AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 // Execute tests based on method name class
-@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DepartmentControllerTest {
 
     @Autowired
@@ -50,7 +55,6 @@ public class DepartmentControllerTest {
     private ObjectMapper objectMapper;
     List<Department> departments =null;
     Department departmentToAdd1 = null;
-
     Department departmentToAdd2 = null;
 
     @BeforeEach
@@ -85,34 +89,44 @@ public class DepartmentControllerTest {
                 .build();
 
         // id = 0, because our controller is defined return the same object that we passed  (id=0 in order to create the new department)
-        departmentToAdd1 = new Department(0,"Marketing", "marketing@mail.com");
-        departmentToAdd2 = new Department(0, "Sales", "sales@example.com");
+        // id = 1 or 2 will work since controller handle id different to zero
+        departmentToAdd1 = new Department(1,"Marketing", "marketing@mail.com");
+        departmentToAdd2 = new Department(2, "Sales", "sales@example.com");
 
-        departments=Stream.of(new Department(1,"Marketing", "marketing@mail.com"),
-                        new Department(2,"Sales", "sales@example.com"))
-                .collect(Collectors.toList());
+        departments=Stream.of(departmentToAdd1, departmentToAdd2)
+                          .collect(Collectors.toList());
     }
 
     @Test
+    @Order(1)
     public void testAddDepartment1() throws Exception {
         String departmentJson = new ObjectMapper().writeValueAsString(departmentToAdd1);
         this.mockMvc.perform(post("/api/departments")
                         .content(departmentJson)
                         .contentType("application/json"))
                 .andExpect(status().isCreated() )
-                .andExpect(MockMvcResultMatchers.content().json(departmentJson));
+                // Avoid ID verification since 
+                .andExpect( jsonPath("$.name").value(departmentToAdd1.getName()) )
+                .andExpect( jsonPath("$.email").value(departmentToAdd1.getEmail()) );
+                //.andExpect(MockMvcResultMatchers.content().json(departmentJson));
     }
 
     @Test
+    @Order(2)
     public void testAddDepartment2() throws Exception {
         String departmentJson = new ObjectMapper().writeValueAsString(departmentToAdd2);
         this.mockMvc.perform(post("/api/departments")
                         .content(departmentJson)
                         .contentType("application/json"))
                 .andExpect(status().isCreated() )
-                .andExpect(MockMvcResultMatchers.content().json(departmentJson));
+                // Avoid id verification
+                .andExpect( jsonPath("$.name").value(departmentToAdd2.getName()) )
+                .andExpect( jsonPath("$.email").value(departmentToAdd2.getEmail()) ); 
+                //.andExpect(MockMvcResultMatchers.content().json(departmentJson));
     }
+  
     @Test
+    @Order(3)
     public void testGetDepartments() throws Exception {
         this.mockMvc.perform(get("/api/departments")
                         .contentType("application/json"))
@@ -120,6 +134,43 @@ public class DepartmentControllerTest {
                 .andExpect(MockMvcResultMatchers.content().json(new ObjectMapper().writeValueAsString(departments)));
     }
 
+    @Test
+    @Order(4)
+    public void testUpdateDepartment() throws Exception {
+      // Update one field of the Department 2
+      departmentToAdd2.setEmail("updatedemail@example.com"); 
+      String departmentJson = new ObjectMapper().writeValueAsString(departmentToAdd2);
 
+      this.mockMvc.perform(
+                      put("/api/departments")
+                      .content(departmentJson)
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .accept(MediaType.APPLICATION_JSON)
+                  )
+                  .andExpect( status().isOk() )
+                  .andExpect( MockMvcResultMatchers.content().json(new ObjectMapper().writeValueAsString(departmentToAdd2)) );
+    }
+
+    @Test
+    @Order(5)
+    public void testDeleteDepartmentOneByIdReturnOk() throws Exception {
+      this.mockMvc.perform(
+                      delete("/api/departments/{id}", "1")
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(status().isOk());
+    }
+    
+    @Test
+    @Order(6)
+    public void testDeleteDepartmentTwoByIdReturnOk() throws Exception {
+      this.mockMvc.perform(
+                      delete("/api/departments/{id}", "2")
+                      .contentType(MediaType.APPLICATION_JSON)
+                      .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(status().isOk());
+    }
 
 }
